3a8bf952622a0b610a18dfe514647ba0ce9790d0
Lindsey Smith <lindsey@puppetlabs.com>
2013-06-03 12:12:39 -0700

    [PE-426] Revert "Add MD5 to list of suitable digests"
    This reverts commit 693d105f3cfb33b6d41ebfb0eb8f1c4bb4543042.
    
    The cert manager is now using the SHA256 fingerprint by default.

--- lib/puppet/ssl/host.rb
+++ lib/puppet/ssl/host.rb
@@ -304,7 +304,7 @@ ERROR_STRING
   # configurable
   # --jeffweiss 29 aug 2012
   def suitable_message_digest_algorithms
-    [:MD5, :SHA1, :SHA256, :SHA512]
+    [:SHA1, :SHA256, :SHA512]
   end
 
   # Attempt to retrieve a cert, if we don't already have one.
