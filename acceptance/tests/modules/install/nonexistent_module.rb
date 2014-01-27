49d0c132f0d927e138cb0e4164c628ce2e403ab7
Jesse Scott <jesse@puppetlabs.com>
2014-01-27 12:36:56 -0800

    Attached tickets to FIXMEs in acceptance tests.

--- acceptance/tests/modules/install/nonexistent_module.rb
+++ acceptance/tests/modules/install/nonexistent_module.rb
@@ -17,7 +17,7 @@ stub_forge_on(master)
 
 step "Try to install a non-existent module"
 on master, puppet("module install #{module_author}-#{module_name}"), :acceptable_exit_codes => [1] do
-  # FIXME this is currently broken.
+  # FIXME PF-345
   # assert_match(/#{module_author}\/#{module_name} not found/, stderr,
   #      "Error that module was not found was not displayed")
 end
@@ -39,11 +39,11 @@ OUTPUT
 
 
   assert_equal nil,                         json['module_version']
-  # FIXME this is currently broken
+  # FIXME PF-356
   #assert_equal "#{module_author}-#{module_name}", json['module_name']
   assert_equal 'failure',                   json['result']
   assert_equal master['distmoduledir'],     json['install_dir']
-  # FIXME this is currently broken
+  # FIXME PF-345
   #assert_equal multiline_expectation,       json['error']['multiline']
   #assert_equal oneline_expectation,         json['error']['oneline']
 end
