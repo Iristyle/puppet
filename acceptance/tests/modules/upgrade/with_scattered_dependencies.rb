0243279a57f0d14257af61fe6d4ef2d06da024c0
Alex Dreyer <alex@puppetlabs.com>
2014-01-26 21:16:47 -0800

    move from postgresql to postql

--- acceptance/tests/modules/upgrade/with_scattered_dependencies.rb
+++ acceptance/tests/modules/upgrade/with_scattered_dependencies.rb
@@ -10,7 +10,7 @@ on master, "mkdir -p #{testdir}/modules"
 
 teardown do
   on master, "rm -rf #{master['distmoduledir']}/java"
-  on master, "rm -rf #{master['distmoduledir']}/postgresql"
+  on master, "rm -rf #{master['distmoduledir']}/postql"
 end
 
 master_opts = {
@@ -22,22 +22,22 @@ master_opts = {
 with_puppet_running_on master, master_opts, testdir do
   on master, puppet("module install pmtacceptance-stdlub --version 0.0.2 --target-dir #{testdir}/modules")
   on master, puppet("module install pmtacceptance-java --version 1.6.0 --target-dir #{master['distmoduledir']} --ignore-dependencies")
-  on master, puppet("module install pmtacceptance-postgresql --version 0.0.1 --target-dir #{master['distmoduledir']} --ignore-dependencies")
+  on master, puppet("module install pmtacceptance-postql --version 0.0.1 --target-dir #{master['distmoduledir']} --ignore-dependencies")
   on master, puppet("module list") do
     assert_match /pmtacceptance-java.*1\.6\.0/, stdout, 'Could not find pmtacceptance/java'
-    assert_match /pmtacceptance-postgresql.*0\.0\.1/, stdout, 'Could not find pmtacceptance/postgresql'
+    assert_match /pmtacceptance-postql.*0\.0\.1/, stdout, 'Could not find pmtacceptance/postql'
     assert_match /pmtacceptance-stdlub.*0\.0\.2/, stdout, 'Could not find pmtacceptance/stdlub'
   end
 
   step "Upgrade a module that has a more recent version published"
-  on master, puppet("module upgrade pmtacceptance-postgresql --version 0.0.2") do
+  on master, puppet("module upgrade pmtacceptance-postql --version 0.0.2") do
     assert_output <<-OUTPUT
-      \e[mNotice: Preparing to upgrade 'pmtacceptance-postgresql' ...\e[0m
-      \e[mNotice: Found 'pmtacceptance-postgresql' (\e[0;36mv0.0.1\e[m) in #{master['distmoduledir']} ...\e[0m
+      \e[mNotice: Preparing to upgrade 'pmtacceptance-postql' ...\e[0m
+      \e[mNotice: Found 'pmtacceptance-postql' (\e[0;36mv0.0.1\e[m) in #{master['distmoduledir']} ...\e[0m
       \e[mNotice: Downloading from https://forgeapi.puppetlabs.com ...\e[0m
       \e[mNotice: Upgrading -- do not interrupt ...\e[0m
       #{master['distmoduledir']}
-      └─┬ pmtacceptance-postgresql (\e[0;36mv0.0.1 -> v0.0.2\e[0m)
+      └─┬ pmtacceptance-postql (\e[0;36mv0.0.1 -> v0.0.2\e[0m)
         ├─┬ pmtacceptance-java (\e[0;36mv1.6.0 -> v1.7.0\e[0m)
         │ └── pmtacceptance-stdlub (\e[0;36mv0.0.2 -> v1.0.0\e[0m) [#{testdir}/modules]
         └── pmtacceptance-stdlub (\e[0;36mv0.0.2 -> v1.0.0\e[0m) [#{testdir}/modules]
