9453104798f165bc7c883582b38976c88ac3951e
Jesse Scott <jesse@puppetlabs.com>
2014-02-05 14:18:01 -0800

    (PF-383) Add unit tests for PMT "list" PE compatibility warnings.

--- spec/unit/face/module/list_spec.rb
+++ spec/unit/face/module/list_spec.rb
@@ -218,5 +218,68 @@ describe "puppet module list" do
         Puppet::Face[:module, :current].list, {:tree => true}
       )
     end
+
+    context "when running PE" do
+      before(:each) do
+        Puppet.stubs(:enterprise?).returns(true)
+        Puppet.stubs(:pe_version).returns('3.2.0')
+      end
+
+      it "should warn when unsatisifed PE requirements are present" do
+        PuppetSpec::Modules.create('pe_dependable', @modpath1, :metadata => {
+          :version => '0.0.5',
+          :requirements => [{
+            "name" => "PE",
+            "version_requirement" => "2.x"
+          }]
+        })
+
+        Puppet.expects(:warning).with(regexp_matches(/'pe_dependable' \(v0\.0\.5\) requires Puppet Enterprise 2\.x/))
+
+        Puppet::Face[:module, :current].list_when_rendering_console(
+          Puppet::Face[:module, :current].list, {:tree => true}
+        )
+      end
+
+      context "when multiple PE requirements are declared on a module" do
+        it "should warn about the first unsatisfied requirement" do
+          PuppetSpec::Modules.create('pe_dependable', @modpath1, :metadata => {
+            :version => '0.0.5',
+            :requirements => [
+              { "name" => "PE", "version_requirement" => "3.x" },
+              { "name" => "PE", "version_requirement" => "1.x" }
+            ]
+          })
+
+          Puppet.expects(:warning).with(regexp_matches(/'pe_dependable' \(v0\.0\.5\) requires Puppet Enterprise 1\.x/))
+
+          Puppet::Face[:module, :current].list_when_rendering_console(
+            Puppet::Face[:module, :current].list, {:tree => true}
+          )
+        end
+      end
+    end
+
+    context "when not running PE" do
+      before(:each) do
+        Puppet.stubs(:enterprise?).returns(false)
+      end
+
+      it "should NOT warn when unsatisifed PE requirements are present" do
+        PuppetSpec::Modules.create('pe_dependable', @modpath1, :metadata => {
+          :version => '0.0.5',
+          :requirements => [{
+            "name" => "PE",
+            "version_requirement" => "2.x"
+          }]
+        })
+
+        Puppet.expects(:warning).never
+
+        Puppet::Face[:module, :current].list_when_rendering_console(
+          Puppet::Face[:module, :current].list, {:tree => true}
+        )
+      end
+    end
   end
 end
