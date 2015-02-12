08c204c13da3d5ac651f9674f6bd89712e3682de
Iristyle <Iristyle@github>
2015-02-11 16:18:28 -0800

    AIO testing temp commit

--- acceptance/setup/aio/pre-suite/010_Install.rb
+++ acceptance/setup/aio/pre-suite/010_Install.rb
@@ -54,4 +54,18 @@ AGENT_PACKAGES = {
 install_packages_on(master, MASTER_PACKAGES)
 install_packages_on(agents, AGENT_PACKAGES)
 
+agents.each do |agent|
+  if agent['platform'] =~ /windows/
+    arch = agent[:ruby_arch] || 'x86'
+    base_url = ENV['MSI_BASE_URL'] || "http://builds.puppetlabs.lan/puppet-agent/#{ENV['SHA']}/artifacts/windows"
+    filename = ENV['MSI_FILENAME'] || "puppet-agent-#{ENV['VERSION']}-#{arch}.msi"
+    # TESTING with http://int-resources.corp.puppetlabs.net/aio/puppet-agent-1.0.0-aio-ga354e25-x64.msi
+    # bundle exec rake ci:test:aio CONFIG=config/nodes/win2012r2-rubyx64.yaml SHA=8642ba76b6166b2bb788a9a6f7e62ee46db2234f VERSION=1.0.0-aio-ga354e25 MSI_BASE_URL=http://int-resources.corp.puppetlabs.net/aio
+    # MSI_BASE_URL = http://int-resources.corp.puppetlabs.net/aio
+    # VERSION = 1.0.0-aio-ga354e25
+
+    install_puppet_from_msi(agent, :url => "#{base_url}/#{filename}")
+  end
+end
+
 configure_gem_mirror(hosts)
--- acceptance/setup/packages/pre-suite/010_Install.rb
+++ acceptance/setup/packages/pre-suite/010_Install.rb
@@ -51,8 +51,14 @@ install_packages_on(agents, AGENT_PACKAGES)
 agents.each do |agent|
   if agent['platform'] =~ /windows/
     arch = agent[:ruby_arch] || 'x86'
-    base_url = "http://builds.puppetlabs.lan/puppet/#{ENV['SHA']}/artifacts/windows"
-    filename = "puppet-agent-#{ENV['VERSION']}-#{arch}.msi"
+    # needs to change to puppet-agent when MSI building pipeline is correct
+    # base_url = "http://builds.puppetlabs.lan/puppet-agent/#{ENV['SHA']}/artifacts/windows"
+    # base_url = "http://builds.puppetlabs.lan/puppet/#{ENV['SHA']}/artifacts/windows"
+    base_url = ENV['MSI_BASE_URL'] || "http://builds.puppetlabs.lan/puppet/#{ENV['SHA']}/artifacts/windows"
+    filename = ENV['MSI_FILENAME'] || "puppet-agent-#{ENV['VERSION']}-#{arch}.msi"
+    # TESTING with http://int-resources/aio/puppet-agent-1.0.0-aio-ga354e25-x64.msi
+    # MSI_BASE_URL = http://int-resources/aio
+    # VERSION = 1.0.0-aio-ga354e25
 
     install_puppet_from_msi(agent, :url => "#{base_url}/#{filename}")
   end
